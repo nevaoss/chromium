e85c435148f6a724f0082d590d033aba1d03389b
Ekaterina Kulikova <kate.kulikova@lge.com>
2026-05-18 16:12:59 +0300

    [upstream][sync_up] Resolve conflicts after merging '149.0.7827.0~1' into neva-main-rolling
    Conflicts:
        components/autofill/core/browser/webdata/payments/payments_autofill_table.cc
        third_party/blink/public/platform/web_text_input_type.h
        third_party/blink/renderer/core/editing/ime/input_method_controller.cc
        third_party/blink/renderer/core/exported/web_view_impl.cc
        ui/base/ime/text_input_flags.h
    
    //components/autofill/core/browser/webdata/payments/payments_autofill_table.cc
    Trivial conflict after the upstream CL https://crrev.com/c/7791777 where
    refactored SQL helper usage to point to new sql/ utils. Neva supports
    Clang and doesn't use GCC since M142, so removed Neva GCC workarounds
    as no more needed.
    
    //third_party/blink/public/platform/web_text_input_type.h
    //third_party/blink/renderer/core/editing/ime/input_method_controller.cc
    //ui/base/ime/text_input_flags.h
    Trivial conflicts after the upstream CL https://crrev.com/c/7803794
    where introduced a new text input flag
    TEXT_INPUT_FLAG_HAS_BEEN_CUSTOM_PASSWORD. Adapted Neva code accordingly.
    
    //third_party/blink/renderer/core/exported/web_view_impl.cc
    Trivial conflict after the upstream CL https://crrev.com/c/7802271 where
    fixed system-ui font resolution on ChromeOS near Neva insertion.
    
    Issue: NEVA-11214

--- third_party/blink/renderer/core/editing/ime/input_method_controller.cc
+++ third_party/blink/renderer/core/editing/ime/input_method_controller.cc
@@ -1896,7 +1896,16 @@ int InputMethodController::TextInputFlags() const {
       flags |= kWebTextInputFlagHasBeenPasswordField;
   }
 
-<<<<<<< HEAD
+  if (element->HasBeenHeuristicCustomPasswordCSS()) {
+    flags |= kWebTextInputFlagHasBeenCustomPassword;
+  }
+
+  if (auto* text_control = DynamicTo<TextControlElement>(element)) {
+    if (text_control->HasBeenHeuristicCustomPasswordJS()) {
+      flags |= kWebTextInputFlagHasBeenCustomPassword;
+    }
+  }
+
 #if BUILDFLAG(IS_NEVA_APPRUNTIME)
   if (IsA<HTMLInputElement>(element)) {
     const AtomicString& sensitive =
@@ -1917,17 +1926,6 @@ int InputMethodController::TextInputFlags() const {
       flags |= kWebTextInputFlagSystemKeyboardOn;
   }
 #endif
-=======
-  if (element->HasBeenHeuristicCustomPasswordCSS()) {
-    flags |= kWebTextInputFlagHasBeenCustomPassword;
-  }
-
-  if (auto* text_control = DynamicTo<TextControlElement>(element)) {
-    if (text_control->HasBeenHeuristicCustomPasswordJS()) {
-      flags |= kWebTextInputFlagHasBeenCustomPassword;
-    }
-  }
->>>>>>> 149.0.7827.0~1
 
   return flags;
 }
