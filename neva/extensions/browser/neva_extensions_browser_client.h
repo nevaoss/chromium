03e225232576412ac5fabdc324f5f7b51eb99a99
Ekaterina Kulikova <kate.kulikova@lge.com>
2026-05-20 16:11:50 +0300

    [appruntime][neva_extensions] 150.0.7835.0~1: Adapt to use content::ChildProcessId
    In the upstream CL https://crrev.com/c/7806511 int type
    of the |child_id| parameter has been replaced with
    |content::ChildProcessId| type in
    ExtensionsBrowserClient::AllowCrossRendererResourceLoad() which
    is overriden by NevaExtensionsBrowserClient.
    So, adapted AllowCrossRendererResourceLoad() method of
    NevaExtensionsBrowserClient class accordingly.
    
    Also in that upstream CL int type of the |render_process_id| parameter
    has been replaced with |content::ChildProcessId| type in
    CreateExtensionURLLoaderFactory() in extensions namespace.
    So, adapted call of this method in
    AppRuntimeContentBrowserClient::RegisterNonNetworkSubresourceURLLoaderFactories()
    accordingly.
    
    Without this patch the build fails with errors:
    ../../neva/extensions/browser/neva_extensions_browser_client.h:97:33: error: non-virtual member function marked 'override' hides virtual member function
       97 |       const GURL& upstream_url) override;
          |                                 ^
    ../../neva/app_runtime/browser/app_runtime_content_browser_client.cc:732:26: error: no viable conversion from 'int' to 'content::ChildProcessId' (aka 'base::IdType<class ChildProcessIdTag, int, kInvalidChildProcessUniqueId, 1, 0>')
      732 |                          render_process_id, render_frame_id));
          |                          ^~~~~~~~~~~~~~~~~
    
    Issue: NEVA-11223

--- neva/extensions/browser/neva_extensions_browser_client.h
+++ neva/extensions/browser/neva_extensions_browser_client.h
@@ -89,7 +89,7 @@ class NevaExtensionsBrowserClient : public extensions::ExtensionsBrowserClient {
       const network::ResourceRequest& request,
       network::mojom::RequestDestination destination,
       ui::PageTransition page_transition,
-      int child_id,
+      content::ChildProcessId child_id,
       bool is_incognito,
       const extensions::Extension* extension,
       const extensions::ExtensionSet& extensions,
