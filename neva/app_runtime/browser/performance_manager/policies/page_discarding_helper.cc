118aed5a2ba85fddd1adf6c3aa907fbc561ac98a
홍길동 <hong.kildong@fake.tld>
2026-06-23 22:53:54 +0000

    [ai] 150.0.7835.0~1: adaptation

--- neva/app_runtime/browser/performance_manager/policies/page_discarding_helper.cc
+++ neva/app_runtime/browser/performance_manager/policies/page_discarding_helper.cc
@@ -378,7 +378,7 @@ PageDiscardingHelper::CanDiscardResult PageDiscardingHelper::CanDiscard(
 
   // The enterprise policy to except pages from discarding applies to both
   // proactive and urgent discards.
-  if (IsPageOptedOutOfDiscarding(page_node->GetBrowserContextID(),
+ if (IsPageOptedOutOfDiscarding(page_node->GetBrowserContextID().DebugString(),
                                  main_frame_url)) {
     return CanDiscardResult::kProtected;
   }
