d9e1bd7cfc1b799ee1c8acfdd1de6ce3502b321c
Ivan Pavlotsky <ivan.pavlotsky@lge.com>
2026-07-13 13:33:42 +0300

    [appruntime] 151.0.7895.0~1: Add missing |request_url| to the OnBeforeSendHeaders()
    In the upstream CL https://crrev.com/c/7885325 added |request_url| param
    to the signature ofOnBeforeSendHeaders() in
    //services/network/public/mojom/network_context.mojom.
    The override of this func in the Neva's
    AppRuntimeProxyingURLLoaderFactory class is adapted accordingly.
    
    Without this change, the build fails with the following error:
    ../../neva/app_runtime/browser/net/app_runtime_proxying_url_loader_factory.h:123:68: error: non-virtual member function marked 'override' hides virtual member function
      123 |                              OnBeforeSendHeadersCallback callback) override;
          |                                                                    ^
    gen/services/network/public/mojom/network_context.mojom.h:272:16: note: hidden overloaded virtual function 'network::mojom::TrustedHeaderClient::OnBeforeSendHeaders' declared here: different number of parameters (3 vs 2)
      272 |   virtual void OnBeforeSendHeaders(const ::GURL& request_url, const ::net::HttpRequestHeaders& headers, OnBeforeSendHeadersCallback callback) = 0;
          |                ^
    
    Issue: NEVA-11310

--- neva/app_runtime/browser/net/app_runtime_proxying_url_loader_factory.h
+++ neva/app_runtime/browser/net/app_runtime_proxying_url_loader_factory.h
@@ -119,7 +119,8 @@ class AppRuntimeProxyingURLLoaderFactory
         mojo::PendingReceiver<network::mojom::TrustedHeaderClient> receiver);
 
     // network::mojom::TrustedHeaderClient:
-    void OnBeforeSendHeaders(const net::HttpRequestHeaders& headers,
+    void OnBeforeSendHeaders(const GURL& request_url,
+                             const net::HttpRequestHeaders& headers,
                              OnBeforeSendHeadersCallback callback) override;
     void OnHeadersReceived(const std::string& headers,
                            const net::IPEndPoint& endpoint,
